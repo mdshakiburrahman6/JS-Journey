ad7c9d107c712cfa02492522e551f673883638f9
mdshakiburrahman6 <mdshakiburrahman6@gmail.com>
2025-12-10 00:12:25 +0600

    Learn JS Ternary

---
+++ ternary.js
@@ -0,0 +1,18 @@
+let price = 100;
+const isLeader = true;
+
+// if( isLeader === true){
+//     if(price > 1000){
+//         price = price / 2;
+//         console.log(price);
+//     }else{
+//         console.log(price = 0);
+//     }
+// }else{
+//     price = price + 100;
+//     console.log(price);
+// }
+
+
+price = isLeader === true ? price = price > 1000 ? console.log(price = price / 2) : console.log(price = 0) : console.log(price = price + 100); 
+
