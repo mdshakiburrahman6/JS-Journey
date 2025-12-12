4a3312457390b99f442c007547c47b59987d93dd
mdshakiburrahman6 <mdshakiburrahman6@gmail.com>
2025-12-12 23:51:56 +0600

    Learn while  Loop

--- loop.js
+++ loop.js
@@ -1,6 +1,15 @@
-const numbers = [12, 54, 11 , 99, 53, 16, 28];
+// const numbers = [12, 54, 11 , 99, 53, 16, 28];
 
-for (num of numbers){
-    num = num + 100;
-    console.log(num);
+// for (const num of numbers){
+//     num = num + 100;
+//     console.log(num);
+// }
+
+
+
+let num = 0;
+
+while (num < 10){
+    console.log('This is while Loop.', num);
+    num = num + 1;
 }
