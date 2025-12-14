8480dfa812974fa364a4da2d97059605b97d337f
mdshakiburrahman6 <mdshakiburrahman6@gmail.com>
2025-12-14 18:50:05 +0600

    Practive while loop

--- even.js
+++ even.js
@@ -1,3 +1,12 @@
-for (let i = 0; i<=10; i++){
-    console.log(i);
+// for (let i = 0; i<=10; i++){
+//     console.log(i);
+// }
+
+let num = 0;
+let sum = 0;
+
+while (num <=10){
+    sum = sum + num;
+    num++;
 }
+console.log('The summation is:',sum);
