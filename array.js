b32d8c077cc3f26582edccd831b049aee9bdac52
mdshakiburrahman6 <mdshakiburrahman6@gmail.com>
2025-12-17 22:08:31 +0600

    Learn Array, push, pop, shift, unshift

---
+++ array.js
@@ -0,0 +1,19 @@
+let array = [8, 9, 7, 89, 9];
+
+array.push(40);
+console.log(array);
+
+
+array.pop()
+console.log(array);
+
+
+array.shift()
+console.log(array);
+
+
+array.unshift(5)
+console.log(array);
+
+
+
